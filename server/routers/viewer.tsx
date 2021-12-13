a80ad2a73e6ad2d893c97c9c09d2b42407f2a9ba
Mike Casey <mcasey8540@gmail.com>
2021-12-13 17:39:06 -0500

    CU-2069pcb - Update Vulcan Event Types page for non-admin users

--- server/routers/viewer.tsx
+++ server/routers/viewer.tsx
@@ -123,6 +123,7 @@ const loggedInViewerRouter = createProtectedRouter()
         },
         select: {
           id: true,
+          email: true,
           username: true,
           name: true,
           startTime: true,
@@ -254,8 +255,9 @@ const loggedInViewerRouter = createProtectedRouter()
           eventTypes: membership.team.eventTypes,
         }))
       );
-
-      const canAddEvents = user.plan !== "FREE" || eventTypeGroups[0].eventTypes.length < 1;
+      
+      const isAdminUser = process.env.THETIS_ADMIN_USER_EMAILS?.split(";").includes(user.email);
+      const canAddEvents = isAdminUser // user.plan !== "FREE" || eventTypeGroups[0].eventTypes.length < 1;
 
       return {
         viewer: {
