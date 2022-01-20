3fa515f32f9d1398d4fd9ac405eb9099587da43c
Brady Ashby <brady@theskills.com>
2022-01-20 14:21:57 -0700

    set default 24 hours min booking notice

--- pages/getting-started.tsx
+++ pages/getting-started.tsx
@@ -54,6 +54,7 @@ export default function Onboarding(props: inferSSRProps<typeof getServerSideProp
       length: 30,
       locations: [{ type: "integrations:daily" }],
       disableGuests: true,
+      minimumBookingNotice: 1440,
     },
   ];
 
