fda80dabb20bd93ed04062d16218727ada758ed2
Zach Trayner <zach@theskills.com>
2022-03-02 10:50:01 -0600

    switch to hubspot private app oauth

--- services/hubspot.ts
+++ services/hubspot.ts
@@ -8,8 +8,10 @@ interface ISendEvent {
 const updateContact = async ({ data, email }: ISendEvent) => {
   const options = {
     method: "POST",
-    qs: { hapikey: process.env.HUBSPOT_API_KEY },
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.HUBSPOT_OAUTH_KEY}`,
+    },
     body: JSON.stringify({
       properties: Object.entries(data).map(([key, value]) => ({ property: key, value })),
     }),
@@ -17,7 +19,7 @@ const updateContact = async ({ data, email }: ISendEvent) => {
   };
 
   const response = await fetch(
-    `https://api.hubapi.com/contacts/v1/contact/createOrUpdate/email/${email}/?hapikey=${process.env.HUBSPOT_API_KEY}`,
+    `https://api.hubapi.com/contacts/v1/contact/createOrUpdate/email/${email}`,
     options
   );
   const responseData = await response.json();
