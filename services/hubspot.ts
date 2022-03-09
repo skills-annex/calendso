e63548a2b62c15566ce56724b2cadc0f7969da9e
Zach Trayner <zach@theskills.com>
2022-03-09 10:35:36 -0600

    send hubspot data for contacts who recently completed a 1-on-1 via cron job

--- services/hubspot.ts
+++ services/hubspot.ts
@@ -1,8 +1,22 @@
 interface ISendEvent {
   data: {
-    [property: string]: string;
+    [property: string]: string | number;
   };
   email: string;
+}
+
+interface IGetContact {
+  vid: number;
+  "canonical-vid": number;
+  "merged-vids": number[];
+  "portal-id": number;
+  "is-contact": boolean;
+  "profile-url": string;
+  properties: {
+    [property: string]: {
+      value: string;
+    };
+  };
 }
 
 const updateContact = async ({ data, email }: ISendEvent) => {
@@ -24,6 +38,21 @@ const updateContact = async ({ data, email }: ISendEvent) => {
   );
   const responseData = await response.json();
   return responseData;
+};
+
+export const getContact = async (email: string) => {
+  const options = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.HUBSPOT_OAUTH_KEY}`,
+    },
+  };
+
+  const response = await fetch(`https://api.hubapi.com/contacts/v1/contact/email/${email}/profile`, options);
+  const contact: IGetContact | null = await response.json();
+
+  return contact;
 };
 
 export default updateContact;
